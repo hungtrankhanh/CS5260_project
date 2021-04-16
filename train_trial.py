e9f12c0fa1015615c8ef331dadb4c5d4262d9cd5
Hung Tran <hung.trankhanh09@gmail.com>
2021-04-17 00:48:49 +0800

    revert to orginal louth code

--- train_trial.py
+++ train_trial.py
@@ -97,44 +97,44 @@ def main():
     writer_dict["train_global_steps"]=0
     writer_dict["valid_global_steps"]=0
 
-    # best = 1e4
-    # for epoch in range(args.max_epoch):
-    #
-    #     train(args, gen_net = gen_net, dis_net = dis_net, gen_optimizer = gen_optimizer, dis_optimizer = dis_optimizer, gen_avg_param = None, train_loader = train_loader,
-    #         epoch = epoch, writer_dict = writer_dict, fixed_z = None, schedulers=[gen_scheduler, dis_scheduler])
-    #
-    #     checkpoint = {'epoch':epoch, 'best_fid':best}
-    #     checkpoint['gen_state_dict'] = gen_net.state_dict()
-    #     checkpoint['dis_state_dict'] = dis_net.state_dict()
-    #     score = validate(args, None, fid_stat, epoch, gen_net, writer_dict, clean_dir=True)
-    #     # print these scores, is it really the latest
-    #     if epoch%args.val_freq==0:
-    #         save_checkpoint(checkpoint, is_best=(score<best), output_dir=args.output_dir)
-    #         print("Saved Latest Model!")
-    #         if score<best: best=score
-    #
-    # checkpoint = {'epoch':epoch, 'best_fid':best}
-    # checkpoint['gen_state_dict'] = gen_net.state_dict()
-    # checkpoint['dis_state_dict'] = dis_net.state_dict()
-    # score = validate(args, None, fid_stat, epoch, gen_net, writer_dict, clean_dir=True)
-    # save_checkpoint(checkpoint, is_best=(score<best), output_dir=args.output_dir)
+    best = 1e4
+    for epoch in range(args.max_epoch):
+
+        train(args, gen_net = gen_net, dis_net = dis_net, gen_optimizer = gen_optimizer, dis_optimizer = dis_optimizer, gen_avg_param = None, train_loader = train_loader,
+            epoch = epoch, writer_dict = writer_dict, fixed_z = None, schedulers=[gen_scheduler, dis_scheduler])
+
+        checkpoint = {'epoch':epoch, 'best_fid':best}
+        checkpoint['gen_state_dict'] = gen_net.state_dict()
+        checkpoint['dis_state_dict'] = dis_net.state_dict()
+        score = validate(args, None, fid_stat, epoch, gen_net, writer_dict, clean_dir=True)
+        # print these scores, is it really the latest
+        if epoch%args.val_freq==0:
+            save_checkpoint(checkpoint, is_best=(score<best), output_dir=args.output_dir)
+            print("Saved Latest Model!")
+            if score<best: best=score
+
+    checkpoint = {'epoch':epoch, 'best_fid':best}
+    checkpoint['gen_state_dict'] = gen_net.state_dict()
+    checkpoint['dis_state_dict'] = dis_net.state_dict()
+    score = validate(args, None, fid_stat, epoch, gen_net, writer_dict, clean_dir=True)
+    save_checkpoint(checkpoint, is_best=(score<best), output_dir=args.output_dir)
 
     #load model
-
-    best_model = torch.load("best_model")
-    load_epoch = best_model['epoch']
-    best_fid = best_model['best_fid']
-    gen_net.load_state_dict(best_model['gen_state_dict'])
-    dis_net.load_state_dict(best_model['dis_state_dict'])
-    fixed_z = torch.cuda.FloatTensor(np.random.normal(0, 1, (64, args.latent_dim)))
-
-    fid_score = validate(args, None, fid_stat, load_epoch, gen_net, writer_dict, clean_dir=True)
-    print(f'FID score: {fid_score} || @ epoch {load_epoch}.')
-    fake_imgs = gen_net(fixed_z, load_epoch).detach()
-
-    batch_num = len(fake_imgs)
-    for j in range(batch_num):
-        save_image(fake_imgs[j], 'output/gen_image_{}.png'.format(j))
+    #
+    # best_model = torch.load("best_model")
+    # load_epoch = best_model['epoch']
+    # best_fid = best_model['best_fid']
+    # gen_net.load_state_dict(best_model['gen_state_dict'])
+    # dis_net.load_state_dict(best_model['dis_state_dict'])
+    # fixed_z = torch.cuda.FloatTensor(np.random.normal(0, 1, (64, args.latent_dim)))
+    #
+    # fid_score = validate(args, None, fid_stat, load_epoch, gen_net, writer_dict, clean_dir=True)
+    # print(f'FID score: {fid_score} || @ epoch {load_epoch}.')
+    # fake_imgs = gen_net(fixed_z, load_epoch).detach()
+    #
+    # batch_num = len(fake_imgs)
+    # for j in range(batch_num):
+    #     save_image(fake_imgs[j], 'output/gen_image_{}.png'.format(j))
 
         
 if __name__=="__main__":
